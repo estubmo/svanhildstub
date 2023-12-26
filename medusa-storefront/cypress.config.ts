4e6f3674f62575cefcf36e15820dd6733651fbed
Eirik Mo <eirik@mowebdev.com>
2023-12-26 18:22:39 +0100

    feat: update cypress

---
+++ medusa-storefront/cypress.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  env: {
+    codeCoverage: {
+      url: "/api/__coverage__",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
