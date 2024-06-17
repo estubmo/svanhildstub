0b168ef0d850ad97cc9ac691228377513871beff
Eirik Mo <eirik@mowebdev.com>
2024-06-17 22:27:13 +0200

    fix: og:image

--- medusa-storefront/src/app/layout.tsx
+++ medusa-storefront/src/app/layout.tsx
@@ -8,15 +8,22 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://localhost:8000';
 export const metadata: Metadata = {
   metadataBase: new URL(BASE_URL),
   title: 'Svanhild Stub',
+
   description:
     'Svanhild Stub is a Norwegian artist and painter. She is known for her unique abstract work.',
   manifest: `${process.env.NEXT_PUBLIC_BASE_URL}/site.webmanifest`,
   openGraph: {
     title: 'Svanhild Stub - Unique Abstract Art',
+    url: 'https://www.svanhildstub.com',
+    siteName: 'Svanhild Stub',
     description:
       'Learn about Svanhild Stub and browse my gallery of unique abstract art.',
     images: [
-      '/assets/images/ogImage.jpg' 
+      {
+        url: 'https://www.svanhildstub.com/assets/images/ogImage.jpg', // Must be an absolute URL
+        width: 1001,
+        height: 848,
+      },
     ],
   },
   creator: 'Mo Web Dev',
